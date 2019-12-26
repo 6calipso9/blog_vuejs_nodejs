8e6dd6308f5dbc1b1337ec53ba3ab9bfd4ec4692
Ruslan <6calipso9@gmail.com>
2019-12-27 02:11:58 +0300

    change registr form + validation repeat pass + validation name exist

--- backend/src/user/users.service.ts
+++ backend/src/user/users.service.ts
@@ -1,5 +1,5 @@
 import { Model } from 'mongoose';
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpStatus } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from './user.interface';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -10,10 +10,11 @@ export class UsersService {
   constructor(@InjectModel('User') private userModel: Model<User>) {}
 
   async create(createUserDto: CreateUserDto) {
-
-    let createdUser = new this.userModel(createUserDto);
-    return await createdUser.save();
-
+    if(this.userModel.findOne({email: createUserDto.email}).length == 0){
+      let createdUser = new this.userModel(createUserDto);
+      return await createdUser.save();
+    }
+    return {message: "email exist"};
   }
 
   async findOneByEmail(email): Model<User> {
