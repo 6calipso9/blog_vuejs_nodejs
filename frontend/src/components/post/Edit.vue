b61eb4d4de6f9490d373a8ce694d4a097e5492b2
Ruslan <6calipso9@gmail.com>
2019-12-28 04:50:13 +0300

    create edit form

--- frontend/src/components/post/Edit.vue
+++ frontend/src/components/post/Edit.vue
@@ -7,24 +7,21 @@
     </h4>
         <div class="col-md-12 form-wrapper">
           <h2> Edit Post </h2>
+          <div v-show="sucessEdit" class="alert alert-success">
+              <div class="btn-group">
+                <router-link :to="{name: 'Posts', params: {id: id}}" class="btn btn-sm btn-primary ">View Post</router-link>
+              </div>
+                Post edited!
+              </div>
           <form id="edit-post-form" @submit.prevent="editPost">
             <div class="form-group col-md-12">
                 <label for="title"> Title </label>
                 <input type="text" id="title" v-model="post.title" name="title" class="form-control" placeholder="Enter title">
             </div>
             <div class="form-group col-md-12">
-                <label for="description"> Description </label>
-                <input type="text" id="description" v-model="post.description" name="description" class="form-control" placeholder="Enter Description">
+                <label for="text"> Write Content </label>
+                <textarea id="text" cols="30" rows="5" v-model="post.text" class="form-control"></textarea>
             </div>
-            <div class="form-group col-md-12">
-                <label for="body"> Write Content </label>
-                <textarea id="body" cols="30" rows="5" v-model="post.body" class="form-control"></textarea>
-            </div>
-            <div class="form-group col-md-12">
-                <label for="author"> Author </label>
-                <input type="text" id="author" v-model="post.author" name="author" class="form-control">
-            </div>
-
             <div class="form-group col-md-4 pull-right">
                 <button class="btn btn-success" type="submit"> Edit Post </button>
             </div>
@@ -37,11 +34,13 @@
 import { server } from "../../utils/helper";
 import axios from "axios";
 import router from "../../router";
+import { headers } from "../../utils/helper"
 export default {
   data() {
     return {
       id: 0,
-      post: {}
+      post: {},
+      sucessEdit : false
     };
   },
   created() {
@@ -51,24 +50,25 @@ export default {
   methods: {
     editPost() {
       let postData = {
+        ... this.post,
         title: this.post.title,
-        description: this.post.description,
-        body: this.post.body,
-        author: this.post.author,
-        date_posted: this.post.date_posted
+        text: this.post.text,
       };
+      let postik = {post: postData}
+      this.sucessEdit = false
 
-      axios
-        .put(`${server.baseURL}/blog/edit?postID=${this.id}`, postData)
-        .then(data => {
-          console.log(data);
-          router.push({ name: "home" });
-        });
+      this.$http.patch(`${server.baseURL}/posts/${this.id}`, postik, headers).then(data => {
+        console.log(data);
+        this.sucessEdit = true
+        this.id = data.data.post._id
+        this.sucessEdit = true
+        
+      });
     },
     getPost() {
       axios
         .get(`${server.baseURL}/posts/${this.id}`)
-        .then(data => (this.post = data.data));
+        .then(data => {(this.post = data.data.post)});
     },
     navigate() {
       router.go(-1);
