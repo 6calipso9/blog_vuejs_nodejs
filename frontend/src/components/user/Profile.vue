a0ed4a5ea77ee55c6723ec542064ff0a72341275
Ruslan <6calipso9@gmail.com>
2019-12-28 03:10:52 +0300

    recreate profile + add demo table posts

--- frontend/src/components/user/Profile.vue
+++ frontend/src/components/user/Profile.vue
@@ -24,10 +24,53 @@
                 <div class="form-group col-md-12">
                     <label for="text"> Last Update: {{lastUpdate}} </label>
                 </div>  
-                <div class="form-group col-md-4 pull-right">
-                    <button class="btn btn-success" type="submit"> Save </button>
-                </div>    
+                
             </form>
+      </div>
+       <div class="card-footer">
+                <div class="form-group col-md-12 pull-right">
+                    <button class="btn btn-success col-md-12" type="submit"> Save </button>
+                </div>    
+                </div>
+     </div>
+     <div class="card mx-auto mt-3" style="width: 60rem;">
+       <div class="card-header text-center">
+          <h2> My posts </h2>
+       </div>
+     
+      <div class="card-body ">
+        <div id="create-post-form" @submit.prevent="SaveUser">
+                   <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+            </div>
       </div>
      </div>
     </div>
