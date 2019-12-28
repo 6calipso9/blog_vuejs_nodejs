ecb719898e398737777b8c94e3287c541dba9752
William <william@localhost.localdomain>
2019-12-28 04:07:02 +0300

    fix

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,41 +1,42 @@
 <template>
-    <div>
+    <div class="main_style_1">
 
-      <div class="text-center">
-        <h1>BSTU Blog</h1>
-       <p> This blog by student PV-41</p>
-
-       <div v-if="posts.length === 0">
-            <h2> No post found at the moment </h2>
+      <div class="home_title" style="order: 1">
+        <div style="order: 1">
+          <h1>BSTU Blog</h1>
+          <p> This blog by student PV-41</p>
+        </div>
+        <div v-if="posts.length === 0" style="order: 2">
+          <h2> No post found at the moment </h2>
         </div>
       </div>
        
-        <div class="row">
-           <div class="col-md-3" v-for="post in posts" :key="post._id">
-              <div class="card mb-4 shadow-sm">
-                <div class="card-header">
-                   <h2 class="card-img-top">{{ post.title }}</h2>
+      <div class="row" style="order: 2">
+        <div class="col-md-4" v-for="post in posts" :key="post._id">
+          <div class="card mb-4 shadow-sm sp_card">
+            <div class="card-header">
+              <h2 class="card-img-top">{{ post.title }}</h2>
+            </div>
+            <div class="card-body">
+              <p class="card-text">{{ cutText(post.text, 150)}}</p>                                 
+            </div>
+            <div class="card-footer sp_card_footer">
+              <div class="row align-items-center">
+                <div class="col-md-6">
+                  <small class="text-muted">Posted on: {{ post.createdAt}}</small><br/>
+                  <small class="text-muted">by: {{ post.author}}</small>
                 </div>
-                <div class="card-body">
-                  <p class="card-text">{{ cutText(post.text, 150)}}</p>                                 
-                </div>
-                  <div class="card-footer">
-                    <div class="row align-items-center">
-                      <div class="col-md-6">
-                        <small class="text-muted">Posted on: {{ post.createdAt}}</small><br/>
-                        <small class="text-muted">by: {{ post.author}}</small>
-                      </div>
-                      <div class="col-md-6">
-                        <div class="d-flex justify-content-between align-items-center">
-                          <div class="btn-group">
-                            <router-link :to="{name: 'Posts', params: {id: post._id}}" class="btn btn-sm btn-primary ">View Post</router-link>
-                          </div>
-                        </div>  
-                      </div>
+                <div class="col-md-6">
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                      <router-link :to="{name: 'Posts', params: {id: post._id}}" class="btn btn-sm btn-primary ">View Post</router-link>
                     </div>
-                  </div>
+                  </div>  
+                </div>
               </div>
             </div>
+          </div>
+        </div>
       </div>
     </div>
 </template>
