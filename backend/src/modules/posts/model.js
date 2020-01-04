c22f8532a6736c86758bcdd5000fdfd1b20964df
calipso <55281318+6calipso9@users.noreply.github.com>
2020-01-04 20:00:50 +0300

    change form create

--- backend/src/modules/posts/model.js
+++ backend/src/modules/posts/model.js
@@ -6,7 +6,11 @@ module.exports = function () {
   const Post = new Schema({
     author: { type: Schema.Types.ObjectId, ref: 'User', required: true },
     title: { type: String, required: true },
+    typePost: { type: String, required: true },
+    status: { type: String, required: true },
     text: { type: String, required: true },
+    image: { type: String },
+    tags: [{ type: String }],
   }, {
     timestamps: true
   });
--- frontend/src/components/post/Create.vue
+++ frontend/src/components/post/Create.vue
@@ -1,54 +1,119 @@
 <template>
-   <div>
-        <div class="col-md-12 form-wrapper">
-          <form id="create-post-form" @submit.prevent="createPost">
-          <div class="card mx-auto" style="width: 30rem;">
-            <div class="card-header text-center">
-                <h2> Create post </h2>
-            </div>
-            <div class="card-body">
-              
-                <div v-show="sucessAdd" class="alert alert-success">
-                <div class="btn-group">
-                  <router-link :to="{name: 'Posts', params: {id: id}}" class="btn btn-sm btn-primary ">View Post</router-link>
-                </div>
-                  Post added!
-                </div>
-                
-                <div class="form-group col-md-12">
-                  <label for="title"> Title </label>
-                  <input type="text" id="title" v-model="title" name="title" class="form-control" placeholder="Enter title" required="required" pattern="[A-Za-z0-9]{1,20}">
-                </div>
-                <div class="form-group col-md-12">
-                    <label for="body"> Write Content </label>
-                    <textarea id="text" cols="30" rows="5" v-model="text" class="form-control" required="required" pattern="[A-Za-z0-9]{1,20}"></textarea>
-                </div>        
-            
-            </div>
-            <div class="card-footer">
-                <div class="form-group col-md-12 pull-right">
-                    <button class="btn btn-success col-md-12" type="submit"> Create Post </button>
-                </div>    
-                </div>
+  <div>
+    <div class="col-md-12 form-wrapper">
+      <form id="create-post-form" @submit.prevent="createPost">
+        <div class="card mx-auto" style="width: 30rem;">
+          <div class="card-header text-center">
+            <h2>Create post</h2>
           </div>
-          </form>
-          
+          <div class="card-body">
+            <div v-show="sucessAdd" class="alert alert-success">
+              <div class="btn-group">
+                <router-link
+                  :to="{name: 'Posts', params: {id: id}}"
+                  class="btn btn-sm btn-primary"
+                >View Post</router-link>
+              </div>Post added!
+            </div>
+
+            <div class="form-group col-md-12">
+              <label for="title">Title</label>
+              <input
+                type="text"
+                id="title"
+                v-model="title"
+                name="title"
+                class="form-control"
+                placeholder="Enter title"
+                required="required"
+                pattern="[A-Za-z0-9]{1,20}"
+              />
+            </div>
+            <div class="form-group col-md-12">
+              <label for="body">Write Content</label>
+              <textarea
+                id="text"
+                cols="30"
+                rows="5"
+                v-model="text"
+                class="form-control"
+                required="required"
+                pattern="[A-Za-z0-9]{1,20}"
+              ></textarea>
+            </div>
+            <div class="form-group col-md-12">
+              <label for="body">Select type</label>
+              <select class="form-control" id="exampleFormControlSelect1">
+                <option>Post</option>
+                <option>Guide</option>
+                <option>Text</option>
+              </select>
+            </div>
+            <div class="form-group col-md-12">
+              <label for="body">Select status</label>
+              <div class>
+                <div class="form-check form-check-inline">
+                  <input
+                    class="form-check-input"
+                    type="radio"
+                    name="inlineRadioOptions"
+                    id="inlineRadio1"
+                    value="option1"
+                  />
+                  <label class="form-check-label" for="inlineRadio1">Ready</label>
+                </div>
+                <div class="form-check form-check-inline">
+                  <input
+                    class="form-check-input"
+                    type="radio"
+                    name="inlineRadioOptions"
+                    id="inlineRadio2"
+                    value="option2"
+                  />
+                  <label class="form-check-label" for="inlineRadio2">In process</label>
+                </div>
+              </div>
+            </div>
+            <div class="form-group col-md-12">
+              <label for="body">Select background iamge</label>
+              <div class="custom-file">
+                <input type="file" class="custom-file-input" id="customFile" />
+                <label class="custom-file-label" for="customFile">image...</label>
+              </div>
+            </div>
+            <div class="form-group col-md-12">
+              <label for="body">Select tags</label>
+              <v-col cols="12">
+                <v-autocomplete v-model="value" :items="items" dense filled label="Filled"></v-autocomplete>
+              </v-col>
+            </div>
+          </div>
+          <div class="card-footer">
+            <div class="form-group col-md-12 pull-right">
+              <button class="btn btn-success col-md-12" type="submit">Create Post</button>
+            </div>
+          </div>
         </div>
+      </form>
     </div>
+  </div>
 </template>
 
 <script>
 //import axios from "axios";
 import { server } from "../../utils/helper";
 //import router from "../../router";
-import { headers } from "../../utils/helper"
+import { headers } from "../../utils/helper";
 export default {
   data() {
     return {
       title: "",
       text: "",
       sucessAdd: false,
-      id: ""
+      id: "",
+      items: ["foo", "bar", "fizz", "buzz"],
+      values: ["foo", "bar"],
+      value: null
     };
   },
   created() {
@@ -56,22 +121,21 @@ export default {
   },
   methods: {
     createPost() {
-      this.sucessAdd = false
+      this.sucessAdd = false;
       let postData = {
         title: this.title,
-        text: this.text,
+        text: this.text
       };
-      let post = {post: postData}
+      let post = { post: postData };
       this.__submitToServer(post);
     },
     __submitToServer(data) {
       this.$http.post(`${server.baseURL}/posts`, data, headers).then(data => {
         console.log(data);
-        this.sucessAdd = true
-        this.id = data.data.post._id
+        this.sucessAdd = true;
+        this.id = data.data.post._id;
       });
     }
   }
 };
 </script>
-
